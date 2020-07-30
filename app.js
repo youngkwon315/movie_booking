c2f2f2ce1ac69c7d5cab11733d3af9c636758881
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-07-30 23:59:45 +0900

    added js functions

---
+++ app.js
@@ -0,0 +1,32 @@
+//get element from html document
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+let ticketPrice = parseInt(movieSelect.value);
+
+//update count and total
+function updateSelectCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = parseInt(movieSelect.value);
+});
+
+//click event
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateSelectCount();
+  }
+});
