b75bd2d4a46b78b6318133823d9a2a2088bdd367
youngkwon315 <39914288+youngkwon315@users.noreply.github.com>
2020-07-31 23:31:08 +0900

    added localstorage and populate UI

--- app.js
+++ app.js
@@ -5,19 +5,54 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+populateUI();
+
 let ticketPrice = parseInt(movieSelect.value);
+
+//set selected movie index and data
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
 
 //update count and total
 function updateSelectCount() {
+  //선택된 모든 좌석을 selectedSeats에 저장
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  //selectedSeats의 선택된 seat의 index정보를 새로운 배열로 저장
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+  //index정보를 localStorage에 저장
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// get data from local storage populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = parseInt(movieSelect.value);
+  setMovieData(e.target.selectedIndex, e.target.value);
 });
 
 //click event
@@ -30,3 +65,6 @@ container.addEventListener("click", (e) => {
     updateSelectCount();
   }
 });
+
+//initial count and total
+updateSelectCount();
